2340ffb8e6e696ea054ac7e3ded485a61d73a695
MarceloArraes <marcelo.arraes@gmail.com>
2022-07-22 10:31:48 +0000

    improv: some sets on inputs

--- src/screens/Signin.tsx
+++ src/screens/Signin.tsx
@@ -1,5 +1,5 @@
 import { VStack, Heading, Icon, useTheme } from 'native-base';
-import React from 'react'
+import React, {useState} from 'react'
 import Logo from '../assets/logo_primary.svg'
 import {Input} from '../components/Input'
 import { Envelope, Key } from 'phosphor-react-native';
@@ -7,6 +7,9 @@ import { Button } from '../components/Button';
 
 export const Signin = () => {
   const { colors } = useTheme();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
       <Logo />
@@ -15,17 +18,21 @@ export const Signin = () => {
       </Heading>
       <Input
         placeholder="email"
+        value={email}
+        onChangeText={setEmail}
         mb={5} ml={4}
         InputLeftElement={<Icon marginLeft={'2.5'} as={<Envelope color={colors.gray[300]} />} />}
       />
       <Input
         placeholder="senha"
+        value={password}
+        onChangeText={setPassword}
         mb={8} ml={4}
         secureTextEntry
         InputLeftElement={<Icon marginLeft={'2.5'} as={<Key color={colors.gray[300]} />} />}
       />
       <Button title={"MARCELO ARRAES"} mb={4} />
-      <Button title={"Hello World"} mb={4}/>
+      <Button title={"Hello World"} mb={4}  />
     </VStack>
   )
 }
